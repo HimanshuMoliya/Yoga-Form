cfa6ec5be2b7c1fea5f62b7631332afeb5c990ea
Sanya <zaverisanya@gmail.com>
2022-12-11 08:34:36 +0530

    models

---
+++ yogaform/myapp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.core.validators import RegexValidator
+# Create your models here.
+
+class User(models.Model):
+    user_id=models.BigAutoField(primary_key=True)
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    age=models.PositiveIntegerField()
+    email=models.EmailField(max_length=254)
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    contact_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) 
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender=models.CharField(max_length=1, choices=GENDER_CHOICES)
+    batch=models.ForeignKey('Batch',on_delete=models.CASCADE,null=False)
+
+class Batch(models.Model):
+    batch_id=models.BigAutoField(primary_key=True)
+    SESSION_CHOICES = (
+        ('M', '6-7 AM'),
+        ('M', '7-8 AM'),
+        ('M', '8-9 AM'),
+        ('E', '5-6 PM')
+    )
+    time=models.CharField(max_length=20, choices=SESSION_CHOICES, null=False)
+
+class Payment(models.Model):
+    payment_id=models.BigAutoField(primary_key=True)
+    user_id=models.ForeignKey('User',on_delete=models.CASCADE)
+    amount=models.PositiveBigIntegerField(null=False)
+    date=models.DateField(null=False)
+    paid=models.BooleanField()
+
+class Admission(models.Model):
+    admission_id=models.BigAutoField(primary_key=True)
+    user_id=models.ForeignKey('User',on_delete=models.CASCADE)
+    payment_id=models.ForeignKey('Payment',on_delete=models.CASCADE)
+    batch_id=models.ForeignKey('Batch',on_delete=models.CASCADE)
+
