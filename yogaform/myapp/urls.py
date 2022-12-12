2841898be12c9afc8675ec496141750966f95172
Sanya <zaverisanya@gmail.com>
2022-12-12 18:27:17 +0530

    created form and fetch api to send data to backend

--- yogaform/myapp/urls.py
+++ yogaform/myapp/urls.py
@@ -1,6 +1,6 @@
-from django.urls import include, path
+from django.urls import path
 from myapp import views
 
 urlpatterns=[
-    path('', views.form), 
+    path('', views.yogaform,name='yogaform'), 
 ]
--- yogaform/myapp/views.py
+++ yogaform/myapp/views.py
@@ -1,18 +1,17 @@
 from rest_framework import status
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializers import FormSerializer, UserSerializer
 
 @api_view(['GET','POST'])
-def form(request):
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+def yogaform(request):
     if request.method=='POST':
         serializer=FormSerializer(data=request.data)
         if serializer.is_valid():
            FormSerializer.save(request.data)
            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+    
+    return render(request,'form.html',{})
