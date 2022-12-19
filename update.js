4f8441e82dfd7450e819a6d60fecc36cd7756962
Sanya <zaverisanya@gmail.com>
2022-12-19 13:25:27 +0530

    update urls.py

--- update.js
+++ update.js
@@ -15,7 +15,7 @@ document.getElementById("formdata").addEventListener("submit",function(e){
         // myHeaders.append("Authorization", "Bearer <api_key>");
         // myHeaders.append("Content-Type", "application/json");
 
-        fetch('https://web-production-7af1.up.railway.app/update',{
+        fetch('/update',{
             method:'POST',
             body:formData,
             // headers: myHeaders
--- yogaform/myapp/urls.py
+++ yogaform/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns=[
-    path('https://web-production-7af1.up.railway.app/', views.yogaform,name='yogaform'), 
-    path('https://web-production-7af1.up.railway.app/update', views.updateform,name='updateform'), 
+    path('', views.yogaform,name='yogaform'), 
+    path('update', views.updateform,name='updateform'), 
     path('completePayment',views.completePayment,name='completepayment')
 ]
