88b2f5ccad76c4504b010f19669e5230ba7da7df
Sanya <zaverisanya@gmail.com>
2022-12-19 17:38:55 +0530

    added host user and pass

--- yogaform/myapp/serializers.py
+++ yogaform/myapp/serializers.py
@@ -31,8 +31,7 @@ class FormSerializer(serializers.Serializer):
         url_test = 'https://web-production-7af1.up.railway.app/completePayment'
         send_mail('Yoga For Life - Payment Link',
         'Dear '+f_name+',\n\nThanks for enrolling in our yoga class.\nCharges: Rs 500/month\nKindly make the payment with the link given below. You can make the payment within 30 days of enrolment to confirm your admission.\n\nLink:'+url_test+'\n\nThank you and see you soon:)\n\nBest,\nSanya',
-        'zaverisanya@gmail.com',
-        [e],fail_silently=True)
+        'zaverisanya@gmail.com',[e],fail_silently=True)
         print("after_send_email")
         if(p=='True'):
             o=Payment.objects.filter(user_id=u_id)[0]
